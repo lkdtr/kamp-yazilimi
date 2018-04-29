d4a40848abf54d76e05a7013866cc625d8d7b313
Umut Karcı <cediddi@google.com>
2018-04-29 17:00:38 +0300

    removed the second migration related to #67

--- mudur/userprofile/migrations/0020_merge.py
+++ mudur/userprofile/migrations/0020_merge.py
@@ -6,7 +6,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('userprofile', '0019_auto_20160821_1640'),
-        ('userprofile', '0019_auto_20160823_2002'),
     ]
 
     operations = [
