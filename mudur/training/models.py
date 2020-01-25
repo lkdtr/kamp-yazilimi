745c647b60f4f8c2a0b2a9378a0059bbad8610d1
Yasar <yasar@riseup.net>
2020-01-25 17:08:20 +0300

    default yoklama durumu düzenlendi

--- mudur/training/models.py
+++ mudur/training/models.py
@@ -59,11 +59,11 @@ class TrainessCourseRecord(models.Model):
 class TrainessParticipation(models.Model):
     courserecord = models.ForeignKey(TrainessCourseRecord)
     morning = models.CharField(choices=TRAINESS_PARTICIPATION_STATE, verbose_name=_("Morning"), max_length=3,
-                               default='0')
+                               default='2')
     afternoon = models.CharField(choices=TRAINESS_PARTICIPATION_STATE, verbose_name=_("Afternoon"), max_length=3,
-                                 default='0')
+                                 default='2')
     evening = models.CharField(choices=TRAINESS_PARTICIPATION_STATE, verbose_name=_("Evening"), max_length=3,
-                               default='0')
+                               default='2')
     day = models.CharField(verbose_name=_("Day"), max_length=20, default='1')
 
     def __str__(self):
